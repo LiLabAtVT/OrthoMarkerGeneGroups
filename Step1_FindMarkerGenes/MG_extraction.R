486cc9ec90039f4cda5cad0c272f334df2ddf3ab
Tran Chau <70779713+ct-tranchau@users.noreply.github.com>
2025-05-13 01:20:16 -0400

    Delete Step1_FindMarkerGenes directory

--- Step1_FindMarkerGenes/MG_extraction.R
+++
@@ -1,5 +0,0 @@
-# Extract marker genes from a tissue from the file MG_example.csv
-MarkerGenes = function(species_MG_frame, tissue_MG_frame){
-  df = MGs[(MGs$species == species_MG_frame) & (MGs$tissue == tissue_MG_frame),]
-  return(df)
-}
